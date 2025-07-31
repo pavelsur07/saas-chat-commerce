83e8cb799664186979cfb7e1dd7886577581cd81
pavel_novikov <pavelsur07@gmail.com>
2025-07-31 14:01:39 +0300

    update Fixtures

--- site/src/DataFixtures/AppFixtures.php
+++ site/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
+use App\Entity\Message;
 use App\Entity\User;
 use App\Entity\Company;
 use App\Entity\UserCompany;
@@ -62,6 +64,37 @@ class AppFixtures extends Fixture
         $userCompany3 = new UserCompany(Uuid::uuid4()->toString(),$user3, $company2);
         $userCompany3->setRole('operator');
         $manager->persist($userCompany3);
+
+
+        // === Telegram Client ===
+        $telegramClient = new Client(Uuid::uuid4()->toString(),'telegram', '123456789', $company1);
+        $telegramClient->setUsername('telegram_user');
+        $telegramClient->setFirstName('Alex');
+        $telegramClient->setRawData(['chat' => ['id' => 123456789, 'username' => 'telegram_user']]);
+        $manager->persist($telegramClient);
+
+        $manager->persist(new Message(Uuid::uuid4()->toString(),$telegramClient, 'in', 'Привет!'));
+        $manager->persist(new Message(Uuid::uuid4()->toString(),$telegramClient, 'out', 'Здравствуйте, чем могу помочь?'));
+
+        // === WhatsApp Client ===
+        $whatsappClient = new Client(Uuid::uuid4()->toString(),'whatsapp', '79001234567', $company1);
+        $whatsappClient->setFirstName('Ivan');
+        $whatsappClient->setUsername('+7 900 123-45-67');
+        $whatsappClient->setRawData(['wa_id' => '79001234567']);
+        $manager->persist($whatsappClient);
+
+        $manager->persist(new Message(Uuid::uuid4()->toString(),$whatsappClient, 'in', 'Здравствуйте, вы доставляете в Казань?'));
+        $manager->persist(new Message(Uuid::uuid4()->toString(),$whatsappClient, 'out', 'Да, доставка в Казань занимает 2 дня'));
+
+        // === Instagram Client ===
+        $instaClient = new Client(Uuid::uuid4()->toString(),'instagram', 'insta_001', $company2);
+        $instaClient->setUsername('@insta_user');
+        $instaClient->setFirstName('Olga');
+        $instaClient->setRawData(['profile' => ['username' => '@insta_user']]);
+        $manager->persist($instaClient);
+
+        $manager->persist(new Message(Uuid::uuid4()->toString(),$instaClient, 'in', 'Это точно хлопок?'));
+        $manager->persist(new Message(Uuid::uuid4()->toString(),$instaClient, 'out', 'Да, 100% органический хлопок!'));
 
         // Сохранение всех
         $manager->flush();
