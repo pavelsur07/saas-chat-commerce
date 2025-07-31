02b6c2a2538e341dfa0034fdd75575bae93d8722
pavel_novikov <pavelsur07@gmail.com>
2025-07-31 13:30:14 +0300

    added switch company

---
+++ site/src/Controller/CompanyController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use App\Repository\UserCompanyRepository;
+use App\Service\CompanyContextService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class CompanyController extends AbstractController
+{
+    #[Route('/companies', name: 'company.list')]
+    public function index(UserCompanyRepository $repo)
+    {
+        $companies = $repo->findBy(['user' => $this->getUser()]);
+        return $this->render('company/index.html.twig', ['companies' => $companies]);
+    }
+
+    #[Route('/companies/switch/{id}', name: 'company.switch')]
+    public function switch(Company $company, CompanyContextService $context, UserCompanyRepository $repo): RedirectResponse
+    {
+        $userCompany = $repo->findOneBy([
+            'user' => $this->getUser(),
+            'company' => $company
+        ]);
+
+        if (!$userCompany) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $context->setCompany($company);
+        return $this->redirectToRoute('dashboard');
+    }
+}
---
+++ site/src/Service/CompanyContextService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Company;
+use App\Entity\User;
+use App\Repository\UserCompanyRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RequestStack;
+/*use Symfony\Component\Security\Core\Security;*/
+
+class CompanyContextService
+{
+    private ?Company $currentCompany = null;
+
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly Security $security,
+        private readonly UserCompanyRepository $userCompanyRepository
+    ) {}
+
+    public function getCompany(): ?Company
+    {
+        if ($this->currentCompany) {
+            return $this->currentCompany;
+        }
+
+        $session = $this->requestStack->getSession();
+        $companyId = $session->get('active_company_id');
+        if (!$companyId) {
+            return null;
+        }
+
+        /** @var User $user */
+        $user = $this->security->getUser();
+        $userCompany = $this->userCompanyRepository->findOneBy([
+            'user' => $user,
+            'company' => $companyId
+        ]);
+
+        return $this->currentCompany = $userCompany?->getCompany();
+    }
+
+    public function setCompany(Company $company): void
+    {
+        $session = $this->requestStack->getSession();
+        $session->set('active_company_id', $company->getId());
+        $this->currentCompany = $company;
+    }
+}
