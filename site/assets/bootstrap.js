d1ac45583692642e33ae20e58078e9d55d554271
pavelsur07 <pavelsur07@gmail.com>
2025-08-02 09:09:32 +0300

    Register flash controller

--- site/assets/bootstrap.js
+++ site/assets/bootstrap.js
@@ -1,6 +1,8 @@
 // import { startStimulusApp } from '@symfony/stimulus-bundle';
 import { startStimulusApp } from '@symfony/stimulus-bridge';
+import FlashController from './controllers/flash_controller.js';
 
 const app = startStimulusApp();
+app.register('flash', FlashController);
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
