7e3a524e199b683d63e13af211abc9ad06cf72ea
pavelsur07 <pavelsur07@gmail.com>
2025-08-07 20:58:47 +0300

    fix migration order for telegram bots

--- site/migrations/Version20250731150000.php
+++ site/migrations/Version20250731150000.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20240816120000 extends AbstractMigration
+final class Version20250731150000 extends AbstractMigration
 {
     public function getDescription(): string
     {
