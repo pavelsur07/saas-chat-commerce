29fe91845d269723fcfeb3382afbcb0abaf7c923
pavelsur07 <pavelsur07@gmail.com>
2025-09-20 20:14:39 +0300

    Add CRM deal management API

---
+++ site/src/Service/Crm/DealFactory.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Service\Crm;
+
+use App\Entity\Company\Company;
+use App\Entity\Company\User;
+use App\Entity\Crm\CrmDeal;
+use App\Entity\Crm\CrmPipeline;
+use App\Entity\Crm\CrmStage;
+use App\Entity\Messaging\Client;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class DealFactory
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        #[Autowire(service: 'monolog.logger.crm')]
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    /**
+     * @param array<array-key, mixed> $meta
+     */
+    public function create(
+        Company $company,
+        CrmPipeline $pipeline,
+        CrmStage $stage,
+        User $createdBy,
+        string $title,
+        ?string $amount = null,
+        ?Client $client = null,
+        ?User $owner = null,
+        ?string $source = null,
+        array $meta = [],
+        ?DateTimeImmutable $openedAt = null,
+    ): CrmDeal {
+        $now = new DateTimeImmutable();
+        $openedAt ??= $now;
+
+        $deal = new CrmDeal(
+            Uuid::uuid4()->toString(),
+            $company,
+            $pipeline,
+            $stage,
+            $createdBy,
+            $title,
+            $openedAt,
+        );
+
+        $deal->setCreatedAt($now);
+        $deal->setUpdatedAt($now);
+        $deal->setAmount($amount);
+        $deal->setClient($client);
+        $deal->setOwner($owner);
+        $deal->setSource($source);
+        $deal->setMeta($meta);
+
+        $this->em->persist($deal);
+        $this->em->flush();
+
+        $this->logger->info('crm.deal_created', [
+            'dealId' => $deal->getId(),
+            'pipelineId' => $pipeline->getId(),
+            'stageId' => $stage->getId(),
+            'createdBy' => $createdBy->getId(),
+            'ownerId' => $owner?->getId(),
+            'clientId' => $client?->getId(),
+            'source' => $source,
+            'amount' => $amount,
+        ]);
+
+        return $deal;
+    }
+}
