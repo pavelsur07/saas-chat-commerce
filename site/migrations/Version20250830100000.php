9e1d47d9a29fd47ec92897bb5be7fb09c60e0faa
pavelsur07 <pavelsur07@gmail.com>
2025-09-20 18:26:17 +0300

    Fix quoting in CRM stages migration

--- site/migrations/Version20250830100000.php
+++ site/migrations/Version20250830100000.php
@@ -16,11 +16,11 @@ final class Version20250830100000 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE "crm_stages" (id UUID NOT NULL, pipeline_id UUID NOT NULL, name VARCHAR(80) NOT NULL, position INT NOT NULL, color VARCHAR(7) DEFAULT ''#CBD5E1'' NOT NULL, probability SMALLINT DEFAULT 0 NOT NULL, is_start BOOLEAN DEFAULT FALSE NOT NULL, is_won BOOLEAN DEFAULT FALSE NOT NULL, is_lost BOOLEAN DEFAULT FALSE NOT NULL, sla_hours INT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "crm_stages" (id UUID NOT NULL, pipeline_id UUID NOT NULL, name VARCHAR(80) NOT NULL, position INT NOT NULL, color VARCHAR(7) DEFAULT \'#CBD5E1\' NOT NULL, probability SMALLINT DEFAULT 0 NOT NULL, is_start BOOLEAN DEFAULT FALSE NOT NULL, is_won BOOLEAN DEFAULT FALSE NOT NULL, is_lost BOOLEAN DEFAULT FALSE NOT NULL, sla_hours INT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_CRM_STAGES_PIPELINE_ID ON "crm_stages" (pipeline_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_CRM_STAGES_PIPELINE_POSITION ON "crm_stages" (pipeline_id, position)');
-        $this->addSql('COMMENT ON COLUMN "crm_stages".created_at IS ''(DC2Type:datetime_immutable)''');
-        $this->addSql('COMMENT ON COLUMN "crm_stages".updated_at IS ''(DC2Type:datetime_immutable)''');
+        $this->addSql('COMMENT ON COLUMN "crm_stages".created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN "crm_stages".updated_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE "crm_stages" ADD CONSTRAINT FK_CRM_STAGES_PIPELINE FOREIGN KEY (pipeline_id) REFERENCES "crm_pipelines" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
