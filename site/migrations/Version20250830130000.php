922913e4aed96e8f0a918b573c9d32f52f5b4938
pavelsur07 <pavelsur07@gmail.com>
2025-09-21 09:36:39 +0300

    Fix Doctrine migration comment quoting

--- site/migrations/Version20250830130000.php
+++ site/migrations/Version20250830130000.php
@@ -19,7 +19,7 @@ final class Version20250830130000 extends AbstractMigration
         $this->addSql('ALTER TABLE "crm_deals" ADD COLUMN stage_entered_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW()');
         $this->addSql('UPDATE "crm_deals" SET stage_entered_at = opened_at');
         $this->addSql('ALTER TABLE "crm_deals" ALTER COLUMN stage_entered_at DROP DEFAULT');
-        $this->addSql('COMMENT ON COLUMN "crm_deals".stage_entered_at IS ''(DC2Type:datetime_immutable)''');
+        $this->addSql("COMMENT ON COLUMN \"crm_deals\".stage_entered_at IS '(DC2Type:datetime_immutable)'");
     }
 
     public function down(Schema $schema): void
