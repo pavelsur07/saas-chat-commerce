cc1fe8f92fced52624300a8a46c55526393aca05
pavelsur07 <pavelsur07@gmail.com>
2025-10-17 16:43:49 +0300

    Handle missing web chat table gracefully

--- site/src/Repository/WebChat/WebChatSiteRepository.php
+++ site/src/Repository/WebChat/WebChatSiteRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\WebChat;
 use App\Entity\WebChat\WebChatSite;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\NonUniqueResultException;
 
 class WebChatSiteRepository extends ServiceEntityRepository
 {
@@ -15,6 +16,10 @@ class WebChatSiteRepository extends ServiceEntityRepository
 
     public function siteKeyExists(string $siteKey): bool
     {
+        if (!$this->isStorageReady()) {
+            return false;
+        }
+
         return (bool) $this->createQueryBuilder('site')
             ->select('1')
             ->andWhere('site.siteKey = :key')
@@ -22,5 +27,37 @@ class WebChatSiteRepository extends ServiceEntityRepository
             ->setMaxResults(1)
             ->getQuery()
             ->getOneOrNullResult();
+    }
+
+    public function findActiveBySiteKey(string $siteKey): ?WebChatSite
+    {
+        if (!$this->isStorageReady()) {
+            return null;
+        }
+
+        try {
+            return $this->createQueryBuilder('site')
+                ->andWhere('site.siteKey = :key')
+                ->andWhere('site.isActive = true')
+                ->setParameter('key', $siteKey)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getOneOrNullResult();
+        } catch (NonUniqueResultException $exception) {
+            // Уникальный индекс гарантирует, что такое не должно происходить, но на всякий случай
+            return null;
+        }
+    }
+
+    public function isStorageReady(): bool
+    {
+        try {
+            $connection = $this->getEntityManager()->getConnection();
+            $schemaManager = $connection->createSchemaManager();
+
+            return $schemaManager->tablesExist([$this->getClassMetadata()->getTableName()]);
+        } catch (\Throwable) {
+            return false;
+        }
     }
 }
