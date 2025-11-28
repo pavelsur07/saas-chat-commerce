f8a64d0560095ec7ecfd54f74ea8ce15c837b531
pavelsur07 <pavelsur07@gmail.com>
2025-11-28 07:55:49 +0300

    Register webform fields Stimulus controller

--- site/assets/bootstrap.js
+++ site/assets/bootstrap.js
@@ -1,8 +1,10 @@
 // import { startStimulusApp } from '@symfony/stimulus-bundle';
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 import FlashController from './controllers/flash_controller.js';
+import WebformFieldsController from './controllers/webform_fields_controller.js';
 
 const app = startStimulusApp();
 app.register('flash', FlashController);
+app.register('webform-fields', WebformFieldsController);
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
