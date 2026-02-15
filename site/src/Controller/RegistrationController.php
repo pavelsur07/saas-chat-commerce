5d9c57b2990c7109f99400c7c60644d4f3cf0d0c
Pavel Novikov <pavelsur07@gmail.com>
2026-02-15 10:11:57 +0300

    disable registration

--- site/src/Controller/RegistrationController.php
+++ site/src/Controller/RegistrationController.php
@@ -28,26 +28,15 @@ class RegistrationController extends AbstractController
             /** @var string $plainPassword */
             $plainPassword = $form->get('plainPassword')->getData();
 
-            // encode the plain password
+         /*   // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            /*// generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('pavel@gmail.com', 'Bot'))
-                    ->to((string) $user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );*/
-
-            // do anything else you need here, like send an email
-
             $dashboardRedirect = $this->redirectToRoute('dashboard');
 
-            return $security->login($user, 'form_login', 'main', $dashboardRedirect);
+            return $security->login($user, 'form_login', 'main', $dashboardRedirect);*/
         }
 
         return $this->render('security/register.html.twig', [
